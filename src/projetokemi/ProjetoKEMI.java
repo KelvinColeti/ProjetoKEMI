74978b30fb1a58af09a9a73f9cef67ab227b39da
luizm <luizm@DESKTOP-NMKEPJC>
2016-04-29 21:11:18 -0300

    FEITO HOJE DIA 29/04

--- src/projetokemi/ProjetoKEMI.java
+++ src/projetokemi/ProjetoKEMI.java
@@ -15,7 +15,7 @@ public class ProjetoKEMI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ola mundo");
     }
     
 }
